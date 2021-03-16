7d53c7800fe055bf5ecdc18c5ce6f6bf6ffe3259
GeNiaaz <geniaaz1009@gmail.com>
2021-03-16 14:14:59 +0800

    ram monster

--- index.py
+++ index.py
@@ -3,11 +3,21 @@ import re
 import nltk
 from nltk.stem import PorterStemmer
 import sys
+import math
 import getopt
 import os
+import pickle
+
 
 def usage():
     print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
+
+
+try:
+    os.mkdir("txt_files")
+except FileExistsError:
+    ...
+
 
 def build_index(in_dir, out_dict, out_postings):
     """
@@ -21,23 +31,40 @@ def build_index(in_dir, out_dict, out_postings):
     list_of_document_id = os.listdir(in_dir)
     list_of_document_id = sorted([int(doc) for doc in list_of_document_id])
 
+    '''
     # Write all possible document IDs to a separate file for query processing
     all_entries_file = open('all_entries.txt', 'a')
     all_entries_file.write(' '.join([str(doc) for doc in list_of_document_id]))
+    '''
 
     # Variables to change
     case_fold_status = True
     stem_status = True
 
-    final_dict_file = open(out_dict, 'a')
-    final_postings_file = open(out_postings, 'a')
-    length_file = open("length.txt", 'a')
-    
+    # Init txt files
+    final_dict_txt_file = open("txt_files/dictionary.txt", 'a')
+    final_postings_txt_file = open("txt_files/postings.txt", 'a')
+    length_txt_file = open("txt_files/length.txt", 'a')
+    normalise_n_txt_file = open("txt_files/normalise_n.txt", 'a')
+    normalise_n_dict_txt_file = open("txt_files/normalise_n_dict_file.txt", 'a')
+
+    # Init pickle files
+    final_dict_pickle_file = open(out_dict, 'ab')
+    final_postings_pickle_file = open(out_postings, 'ab')
+    length_pickle_file = open("length_pickle.pkl", 'ab')
+    normalise_n_pickle_file = open("normalise_n.pkl", 'ab')
+    normalise_n_dict_pickle_file = open("normalise_n_dict_file.pkl", 'ab')
+
     term_counter = 0
+    normalise_n_index_txt = 0
+    normalise_n_index_pickle = 0
 
     ps = PorterStemmer()
 
     dict_of_terms = {}
+    temp_dict = {}
+    temp_dict_tf_for_docs = {}
+    final_tf_vector_dict = {}
 
     for current_doc_id in list_of_document_id:
         doc_path = in_dir + str(current_doc_id)
@@ -57,6 +84,7 @@ def build_index(in_dir, out_dict, out_postings):
                 if stem_status:
                     term = ps.stem(term)
 
+                # Updating main dictionary of terms
                 if term in dict_of_terms:
                     dict_term_pairs = dict_of_terms[term]
 
@@ -74,24 +102,104 @@ def build_index(in_dir, out_dict, out_postings):
                 else:
                     dict_of_terms[term] = {current_doc_id: 1}
 
-        # Writing length file
+                # Updating temp dict for Length[N]
+                if term in temp_dict:
+                    temp_dict[term] = temp_dict[term] + 1
+
+                else:
+                    temp_dict[term] = 1
+
+        # Writing length txt file
         doc_and_counter = (current_doc_id, term_counter)
-        length_file.write(str(doc_and_counter) + "\n")
+        length_txt_file.write(str(doc_and_counter) + "\n")
+        
+        # Writing pickle length file
+        pickle.dump(doc_and_counter, length_pickle_file)
+
+        # Writing Length[N] txt file
+        list_of_tf = []
+        list_of_normalised_tf = []
+        sum_for_normalizing = 0
+        num_unique_terms = len(temp_dict)
+        for key, value in temp_dict.items():
+            value_to_add = 1 + math.log(value, 10)
+            list_of_tf.append((key, value_to_add))
+
+        for pair in list_of_tf:
+            sum_for_normalizing += pair[1] ** 2
+
+        normalizing_factor = sum_for_normalizing ** 0.5
+
+        for p in list_of_tf:
+            temp = p[1]
+            norm_result = temp / normalizing_factor
+
+            list_of_normalised_tf.append((p[0], norm_result))
+
+        temp_dict_tf_for_docs[current_doc_id] = list_of_normalised_tf
+
         term_counter = 0
+
+    for doc_id, list_pairs in temp_dict_tf_for_docs.items():
+        t_dict = dict(list_pairs)
+        resultant_list = []
+
+        for k, v in dict_of_terms.items():
+            if k in t_dict:
+                num = t_dict[k]
+                to_append = (k, num)
+            else:
+                to_append = (k, 0)
+
+            resultant_list.append(to_append)
+
+        final_tf_vector_dict[doc_id] = resultant_list
+
+    for doc_id, doc_list in final_tf_vector_dict.items():
+        data_to_write = (doc_id, doc_list)
+        print(data_to_write)
+        sys.exit(2)
+        # Writing to txt and pickle files
+        normalise_n_txt_file.write(str(data_to_write) + "\n")
+        pickle.dump(data_to_write, normalise_n_pickle_file)
+
+        # Writing to dict file for normalise
+        normalise_n_dict_txt_file.write(str(normalise_n_index_txt) + "\n")
+        pickle.dump(normalise_n_index_pickle, normalise_n_dict_pickle_file)
+
+        normalise_n_index_txt = normalise_n_txt_file.tell()
+        normalise_n_index_pickle = normalise_n_pickle_file.tell()
+
+        temp_dict.clear()
 
     dict_of_terms = dict(sorted(dict_of_terms.items()))
 
     print("printing to file...")
-    final_dict_index = 0
+    final_dict_index_txt = 0
+    final_dict_index_pickle = 0
     for key, value in dict_of_terms.items():
-        list_of_postings = str(list(value.items()))
+        list_of_postings = list(value.items())
+        list_of_postings_str = str(list(value.items()))
+        doc_freq = len(list_of_postings)
 
-        final_postings_file.write(list_of_postings + "\n")
-        ind = final_postings_file.tell()
+        # Writing to txt file for postings
+        final_postings_txt_file.write(list_of_postings_str + "\n")
+        ind_txt = final_postings_txt_file.tell()
 
-        final_dict_file.write(" " + key + " " + ", " + str(final_dict_index) + "\n")
+        # Writing to txt file for dictionary
+        final_dict_txt_file.write(" " + key +
+                                  " doc freq: " + str(doc_freq) +
+                                  ", pointer: " + str(final_dict_index_txt) + "\n")
 
-        final_dict_index = ind
+        # Writing to pkl file for postings
+        pickle.dump(list_of_postings, final_postings_pickle_file)
+        ind_pickle = final_postings_pickle_file.tell()
+
+        # Writing to pkl for dictionary
+        pickle.dump([key, doc_freq, final_dict_index_pickle], final_dict_pickle_file)
+
+        final_dict_index_txt = ind_txt
+        final_dict_index_pickle = ind_pickle
 
 
 input_directory = output_file_dictionary = output_file_postings = None
@@ -103,11 +211,11 @@ except getopt.GetoptError:
     sys.exit(2)
 
 for o, a in opts:
-    if o == '-i': # input directory
+    if o == '-i':  # input directory
         input_directory = a
-    elif o == '-d': # dictionary file
+    elif o == '-d':  # dictionary file
         output_file_dictionary = a
-    elif o == '-p': # postings file
+    elif o == '-p':  # postings file
         output_file_postings = a
     else:
         assert False, "unhandled option"
--- test.py
+++ test.py
@@ -1,12 +1,37 @@
-dict = {}
+import pickle
 
-dict["hell"] = {1: 5, 2: 43}
+# dict = {}
+#
+# dict["hell"] = {1: 5, 2: 43}
+#
+# a = dict["hell"]
+#
+# a[1] = 6
+# a[2] = 343
+#
+# lis = list(dict["hell"].items())
+#
+# # print(str(lis))
+#
+# file = open("dictionary.pkl", 'rb')
+# f = open("postings.pkl", 'rb')
+#
+# g = open("length_pickle.pkl", 'rb')
+#
+# count = 0
+# try:
+#     while abs != b'':
+#         print(pickle.load(file))
+#         count += 1
+# except EOFError:
+#     print("all done\n")
 
-a = dict["hell"]
+a = [("hell", 4), ("ger", 31), ("apple", 43), ("bear", 10)]
+a = dict(sorted(a))
 
-a[1] = 6
-a[2] = 343
+var = "apple"
+if var in a:
+    print(var)
+else:
+    print("not in there")
 
-lis = list(dict["hell"].items())
-
-print(str(lis))
